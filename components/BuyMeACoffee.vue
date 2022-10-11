bdf4923f0dc527827e989bdb2c7158bfb81651a9
alabort <70921209+alabort@users.noreply.github.com>
2022-10-10 23:03:15 -0400

    2 files modified, 4 deleted

--- components/BuyMeACoffee.vue
+++ components/BuyMeACoffee.vue
@@ -1,8 +1,8 @@
 <template>
-  <a href="https://www.buymeacoffee.com/mdrathik" target="_blank"
+  <a href="" target="_blank"
     ><img
-      src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
-      alt="Buy Me A Coffee"
+      src=""
+      alt=""
       style="height: 60px !important; width: 217px !important"
   /></a>
 </template>
--- data/menu.js
+++ data/menu.js
@@ -1,7 +1,6 @@
 const menu = [
   { name: "Home", path: "/" },
   { name: "Projects", path: "/projects" },
-  { name: "Blog", path: "/blog" },
 ];
 
 export default menu;
